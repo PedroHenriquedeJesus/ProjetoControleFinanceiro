a2c71446f70206283c90f3a5d4a415815128e908
KaykySG <kaykyfsoares@hotmail.com>
2024-09-17 20:25:24 -0300

    Iniciando o Projeto back-end
    Incialização do Spring

---
+++ API-Controle-Financeiro/src/main/java/com/pkv/apicontrolefinanceiro/ApiControleFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.pkv.apicontrolefinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControleFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControleFinanceiroApplication.class, args);
+	}
+
+}
---
+++ API-Controle-Financeiro/src/test/java/com/pkv/apicontrolefinanceiro/ApiControleFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkv.apicontrolefinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
